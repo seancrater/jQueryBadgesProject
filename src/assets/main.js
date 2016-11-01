4859b94fe82fb7b27aa8358c4c51f9b17439c930
seancrater <sean@seancrater.com>
2016-11-01 13:50:27 -0400

    Removing console logs

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,11 +4,9 @@ $(function() {
     dataType: 'jsonp',
     success: function(response) {
       var courses = response.courses.completed;
-      console.log(courses);
       $(courses).each(function() {
         var data = "<div class=\"course\"><h3>" + this.title + "</h3><img src=" + this.badge + " /><a class=\"btn btn-primary\" href=" + this.url + " target=\"_blank\">See Course</a></div>";
         $('#badges').append(data);
-        console.log(this.title);
       });
     }
   });
