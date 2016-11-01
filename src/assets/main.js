41db87cddca63e7c576679ec556113ef22028558
seancrater <sean@seancrater.com>
2016-11-01 13:46:17 -0400

    Adding AJAX request and building data in it

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,15 @@
 $(function() {
-
-  // your code will go here
-
+  $.ajax({
+    url: 'https://www.codeschool.com/users/seancrater.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      var courses = response.courses.completed;
+      console.log(courses);
+      $(courses).each(function() {
+        var data = "<div class=\"course\"><h3>" + this.title + "</h3><img src=" + this.badge + " /><a class=\"btn btn-primary\" href=" + this.url + " target=\"_blank\">See Course</a></div>";
+        $('#badges').append(data);
+        console.log(this.title);
+      });
+    }
+  });
 });
